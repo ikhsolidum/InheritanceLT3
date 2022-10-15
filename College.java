a93ba0454f11ff3201ea39971a9a2f39713de3c8
ikhsolidum <100141624+ikhsolidum@users.noreply.github.com>
2022-10-15 22:49:39 +0800

    Create College.java

---
+++ College.java
@@ -0,0 +1,45 @@
+package com.inheritance.sample;
+
+public class College extends Subject {
+
+    //fields of parent class
+    private String designation;
+    String College;
+    String collegeName;
+    String subjectName;
+    String strAction;
+    Subject subObj = new Subject();
+
+    public College(String designation, String collegeName, String subjectName,String College) {
+        super();
+
+        this.College = College;
+        this.designation = designation;
+        this.collegeName = collegeName;
+        subObj.setSubject(subjectName);
+    }
+
+    public void setDesignation(String designation) {
+        this.designation = designation;
+    }
+
+    public String getDesignation() {
+        return designation;
+    }
+    public String getSubjectName() {
+        return subObj.subject;
+    }
+    public String getCollege() {
+        return College;
+    }
+
+    
+    void does(){
+        System.out.println("Teaching");
+    }
+    
+    private void does(String strAction){
+        this.strAction = strAction;
+        System.out.println("Teaching - " + strAction);
+    }
+}
